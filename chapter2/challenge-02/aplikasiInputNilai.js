dca1d2f57366ed42076848e58811fe4a14ddcae5
Piter <pitertan0120@gmail.com>
2023-03-17 18:14:51 +0700

    commit

--- chapter2/challenge-02/aplikasiInputNilai.js
+++ chapter2/challenge-02/aplikasiInputNilai.js
@@ -1,40 +1,57 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+class NilaiSiswa {
+    constructor() {
+        this.nilai = [];
+        this.rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+        });
+    }
 
-let nilai = [];
+tambahNilai() {
+    this.rl.question('Masukkan nilai siswa: ', (input) => {
+    const nilaiSiswa = parseInt(input);
+    this.nilai.push(nilaiSiswa);
+    this.rl.question('Tambah nilai lagi? (y/n) ', (input) => {
+        if (input === 'y') {
+            this.tambahNilai();
+        } else {
+            this.tampilkanOutput();
+            this.rl.close();
+        }
+        });
+    });
+}
 
-rl.question('Masukkan nilai siswa (pisahkan dengan spasi): ', (input) => {
-    nilai = input.split(' ').map(x => parseInt(x));
-
-  // mencari nilai tertinggi dan terendah
-    const nilaiTertinggi = Math.max(...nilai);
-    const nilaiTerendah = Math.min(...nilai);
+tampilkanOutput() {
+    // mencari nilai tertinggi dan terendah
+    const nilaiTertinggi = Math.max(...this.nilai);
+    const nilaiTerendah = Math.min(...this.nilai);
     console.log(`Nilai tertinggi: ${nilaiTertinggi}`);
     console.log(`Nilai terendah: ${nilaiTerendah}`);
 
-  // mencari rata-rata
-    const totalNilai = nilai.reduce((a, b) => a + b, 0);
-    const rataRata = totalNilai / nilai.length;
+    // mencari rata-rata
+    const totalNilai = this.nilai.reduce((a, b) => a + b, 0);
+    const rataRata = totalNilai / this.nilai.length;
     console.log(`Rata-rata: ${rataRata}`);
 
-  // mencari jumlah siswa lulus dan tidak lulus
-    const lulus = nilai.filter(x => x >= 60);
-    const tidakLulus = nilai.filter(x => x < 60);
+    // mencari jumlah siswa lulus dan tidak lulus
+    const lulus = this.nilai.filter(x => x >= 60);
+    const tidakLulus = this.nilai.filter(x => x < 60);
     console.log(`Jumlah siswa lulus: ${lulus.length}`);
     console.log(`Jumlah siswa tidak lulus: ${tidakLulus.length}`);
 
-  // mengurutkan nilai siswa
-    const nilaiUrut = nilai.sort((a, b) => a - b);
+    // mengurutkan nilai siswa
+    const nilaiUrut = this.nilai.sort((a, b) => a - b);
     console.log(`Nilai siswa (terurut): ${nilaiUrut}`);
 
-  // mencari siswa dengan nilai 90 dan 100
-    const siswa90 = nilai.filter(x => x === 90);
-    const siswa100 = nilai.filter(x => x === 100);
+    // mencari siswa dengan nilai 90 dan 100
+    const siswa90 = this.nilai.filter(x => x === 90);
+    const siswa100 = this.nilai.filter(x => x === 100);
     console.log(`Siswa nilai 90 dan nilai 100 : ${siswa90}, ${siswa100}`);
+    }
+}
 
-    rl.close();
-});
+const nilaiSiswa = new NilaiSiswa();
+nilaiSiswa.tambahNilai();
